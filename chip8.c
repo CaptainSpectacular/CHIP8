e0dffe93bedefc611723ae43eb68738317548cfa
pollockcl <pollock.clee@gmail.com>
2018-08-04 09:53:25 -0600

    changed the memory initialization in the struct

--- chip8.c
+++ chip8.c
@@ -3,16 +3,16 @@
 
 typedef struct ChipState
 {
-    int memory;         // Max memory for CHIP8 programs is 4096 bytes
-    uint16_t pc;        // CHIP8 programs start at 0x200.
-    uint16_t stack_ptr; // Starts at 0xEA0
-    uint8_t v[16];      // Register
+    int      memory[4096]; // Max memory for CHIP8 programs is 4096 bytes
+    uint16_t pc;           // CHIP8 programs start at 0x200.
+    uint16_t stack_ptr;    // Starts at 0xEA0
+    uint8_t  v[16];        // Register
 
 }ChipState;
 
 int main(int argc, char *argv[])
 {
-    ChipState state = { 4096, 0x200, 0xEA0 }; // Initialize with default values
+    ChipState state = { 0, 0x200, 0xEA0 }; // Initialize with default values
     char code = 0x000;
 
     // Temp statement
